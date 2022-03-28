95d83a215e26977cbdbbdbe714adae442c7acbb9
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-03-28 23:55:45 +0700

    add some test from comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+
+
+import java.lang.IllegalArgumentException
 import java.util.*
 import java.lang.Exception as Exception
 
@@ -5,24 +8,41 @@ import java.lang.Exception as Exception
 class Codewars() {
 
     fun main(args: Array<String>) {
-        val rex= sumParts(intArrayOf(-1,-1,-2))
+        val rex = sumParts(intArrayOf(Int.MIN_VALUE, Int.MAX_VALUE, Int.MIN_VALUE))
         rex.forEach { println(it) }
     }
 
     fun sumParts(ls: IntArray): IntArray {
-        val sumPartArray = IntArray(ls.size + 1) { 0 }
+        if (ls.isEmpty()) {
+            throw NumberFormatException("Пустой массив, введите данные")
+        }
+        var count=0
+        if (ls.maxOrNull()!! < 0 && ls.minOrNull()!! == Int.MIN_VALUE) {
+            throw IllegalArgumentException("Слишком маленькое число")
+        }
+        ls.forEach { if(it==Int.MIN_VALUE) count++}
+
+        if(count>1)
+        {
+            throw IllegalArgumentException("Слишком маленькое число")
+        }
+        if (ls.maxOrNull() == Int.MAX_VALUE && ls.minOrNull()!! >= 0) {
+            throw IllegalArgumentException("Слишком большое число")
+        }
+
+        var sumPartArray = IntArray(ls.size + 1) { 0 }
         ls.reverse()
         for (i in 1..ls.size) {
-            if(sumPartArray[i - 1] + ls[i - 1]>Int.MAX_VALUE)
-            {
-
-                return throw Exception ("Слишком большое число")
-
-            }else {
-                sumPartArray[i] = sumPartArray[i - 1] + ls[i - 1]
+            sumPartArray[i] = sumPartArray[i - 1] + ls[i - 1]
+            if (ls.maxOrNull() == Int.MAX_VALUE && sumPartArray[i] == Int.MIN_VALUE || ls[i]== Int.MAX_VALUE) {
+                throw IllegalArgumentException("Слишком большое число")
             }
+
+
         }
         sumPartArray.reverse()
         return sumPartArray
     }
+
+
 }
--- src/test/kotlin/TestSumParts.kt
+++ src/test/kotlin/TestSumParts.kt
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.lang.Exception
 import kotlin.test.assertNotEquals
 import java.io.IOException;
+import kotlin.IllegalArgumentException
 
 @Suppress("UNREACHABLE_CODE")
 class TestSumParts {
@@ -26,11 +28,59 @@ class TestSumParts {
         val expected = intArrayOf(-4,-3,-2,0)
         assertArrayEquals(expected, testSample.sumParts(intArrayOf(-1,-1,-2)))
     }
+    @Test
+    fun checkAnswer_WithHardNumber_ReturnException(){
+        val expected = assertThrows<IllegalArgumentException>{
+            testSample.sumParts(intArrayOf(Int.MAX_VALUE,3,-2,0))
+        }
+        assertEquals("Слишком большое число",expected.message )
+    }
+
+    @Test
+    fun checkAnswer_WithEmptyArray_ReturnException(){
+        val expected = assertThrows<NumberFormatException>{
+            testSample.sumParts(intArrayOf())
+        }
+
+        assertEquals("Пустой массив, введите данные",expected.message )
+    }
 
 
     @Test
     fun checkAnswer_WithSmallNumber_ReturnExpected(){
         val expected = intArrayOf(-2147483628, 20, 19, 16, 10, 0)
         assertArrayEquals(expected, testSample.sumParts(intArrayOf(Int.MIN_VALUE, 1, 3, 6, 10)))
+    }
+
+    @Test
+    fun checkAnswer_WithSmallAndHardNumber_ReturnException(){
+        val expected = assertThrows<IllegalArgumentException>{
+            testSample.sumParts(intArrayOf(Int.MIN_VALUE, Int.MAX_VALUE, 1))
+        }
+        assertEquals("Слишком большое число",expected.message )
+    }
+
+    @Test
+    fun checkAnswer_WithSmallAndTwoHardNumber_ReturnException(){
+        val expected = assertThrows<IllegalArgumentException>{
+            testSample.sumParts(intArrayOf(Int.MIN_VALUE, Int.MAX_VALUE, Int.MAX_VALUE))
+        }
+        assertEquals("Слишком большое число",expected.message )
+    }
+
+    @Test
+    fun checkAnswer_WithTwoSmallAndHardNumber_ReturnException(){
+        val expected = assertThrows<IllegalArgumentException>{
+            testSample.sumParts(intArrayOf(Int.MIN_VALUE, Int.MIN_VALUE, Int.MAX_VALUE))
+        }
+        assertEquals("Слишком маленькое число",expected.message )
+    }
+
+    @Test
+    fun checkAnswer_WithSmallAndNegativeNumber_ReturnException(){
+        val expected = assertThrows<IllegalArgumentException>{
+            testSample.sumParts(intArrayOf(Int.MIN_VALUE, -1, -1))
+        }
+        assertEquals("Слишком маленькое число",expected.message )
     }
 }
